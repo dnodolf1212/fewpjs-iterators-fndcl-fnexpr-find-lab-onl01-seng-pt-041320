309b79f09cbaaf9d49e95ad969120913e0bd4ac1
dnodolf1212 <dnodolf@gmail.com>
2020-11-10 20:08:49 -0800

    commit tree 064dccd0dc86dc8e22c4bf3182c92099300d5753

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const testVar = {}
+//function superbowlWin(): to find() ANY SuperBowl win
+//takes an array as an argument
+//array of objects so record[1][year]
+//record.find(w)
+//return the year (record[1][year]) that there is a (w), else return undefined
 
-function testFunc() {
-  return "hi"
+const record = [
+  {year: "2018", result: "L"},
+  //{year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+
+function superbowlWin(record) {
+   let obj1 = record.find(obj => {
+    return obj.result === "W";
+    });
+    if (obj1) {
+    return obj1.year
+    } else{
+      return undefined
+    }
 }
